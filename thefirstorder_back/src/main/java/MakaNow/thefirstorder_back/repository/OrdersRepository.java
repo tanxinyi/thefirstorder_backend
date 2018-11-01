def15038fc56e89d469e49355c1992fc068e6ba9
Keith <keith.siew.2016@sis.smu.edu.sg>
2018-11-01 13:07:48 +0800

    clear project

--- thefirstorder_back/src/main/java/MakaNow/thefirstorder_back/repository/OrdersRepository.java
+++
@@ -1,9 +0,0 @@
-package MakaNow.thefirstorder_back.repository;
-
-import MakaNow.thefirstorder_back.model.Orders;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrdersRepository extends CrudRepository<Orders, String> {
-}
--- thefirstorder_back/src/main/java/MakaNow/thefirstorder_back/repository/SeatingTableRepository.java
+++
@@ -1,9 +0,0 @@
-package MakaNow.thefirstorder_back.repository;
-
-import MakaNow.thefirstorder_back.model.SeatingTable;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SeatingTableRepository extends CrudRepository<SeatingTable, String> {
-}
