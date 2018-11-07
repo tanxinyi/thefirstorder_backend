df849e77772c679509137fa75be438e6d7273f14
tanxinyi <43104241+tanxinyi@users.noreply.github.com>
2018-11-08 03:37:33 +0800

    reclone

--- src/main/java/MakaNow/thefirstorder_back/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package MakaNow.thefirstorder_back.repository;
-
-import MakaNow.thefirstorder_back.model.Category;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoryRepository extends CrudRepository<Category, Integer> {
-}
--- src/main/java/MakaNow/thefirstorder_back/repository/CustomerOrderRepository.java
+++
@@ -1,9 +0,0 @@
-package MakaNow.thefirstorder_back.repository;
-
-import MakaNow.thefirstorder_back.model.CustomerOrder;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerOrderRepository extends CrudRepository<CustomerOrder, String> {
-}
